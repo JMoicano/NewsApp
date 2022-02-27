d48fbca6f8866c54f431a9f5c65361c9d1287e5d
JMoicano <jmariossilva@gmail.com>
2022-02-27 15:01:02 -0300

    Source list navigability

--- app/src/main/java/dev/jmoicano/newsapp/sourceslist/view/SourcesListActivity.kt
+++ app/src/main/java/dev/jmoicano/newsapp/sourceslist/view/SourcesListActivity.kt
@@ -9,18 +9,23 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
+import dev.jmoicano.newsapp.R
 import dev.jmoicano.newsapp.data.Result
 import dev.jmoicano.newsapp.sourceslist.data.models.SourceResponse
 import dev.jmoicano.newsapp.ui.theme.NewsAppTheme
 
+
+@ExperimentalMaterialApi
 @AndroidEntryPoint
 class SourcesListActivity : ComponentActivity() {
 
@@ -31,11 +36,18 @@ class SourcesListActivity : ComponentActivity() {
         setContent {
             NewsAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    topBar = {
+                        TopAppBar {
+                            Text(text = stringResource(id = R.string.app_name))
+                        }
+                    }
                 ) {
                     SourcesList(result = viewModel.sourcesList.value)
+                    {
+                        TODO("Add navigation")
+                    }
                 }
             }
         }
@@ -47,14 +59,18 @@ class SourcesListActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalMaterialApi
 @Composable
-fun SourcesList(result: Result<List<SourceResponse>>) {
+fun SourcesList(
+    result: Result<List<SourceResponse>>,
+    onSourceClicked: (source: SourceResponse) -> Unit
+) {
     when (result) {
         is Result.Success -> {
             LazyColumn(content = {
                 result.response.forEach { response ->
                     item {
-                        SourceView(sourceResponse = response)
+                        SourceView(sourceResponse = response, onSourceClicked = onSourceClicked)
                     }
                 }
             })
@@ -71,12 +87,21 @@ fun SourcesList(result: Result<List<SourceResponse>>) {
     }
 }
 
+@ExperimentalMaterialApi
 @Composable
-fun SourceView(sourceResponse: SourceResponse) {
+fun SourceView(
+    sourceResponse: SourceResponse,
+    onSourceClicked: (source: SourceResponse) -> Unit
+) {
     val context = LocalContext.current
     Card(
-        modifier = Modifier.padding(10.dp),
-        border = BorderStroke(1.dp, MaterialTheme.colors.onSurface)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colors.onSurface),
+        onClick = {
+            onSourceClicked(sourceResponse)
+        }
     ) {
         Column(modifier = Modifier.padding(10.dp)) {
             Text(text = sourceResponse.name, style = MaterialTheme.typography.h4)
