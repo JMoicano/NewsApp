d50ca0bff65a4938fd112f1c2c47ae8088073854
JMoicano <jmariossilva@gmail.com>
2022-02-27 14:40:52 -0300

    Sources List layout

--- app/src/main/java/dev/jmoicano/newsapp/sourceslist/view/SourcesListActivity.kt
+++ app/src/main/java/dev/jmoicano/newsapp/sourceslist/view/SourcesListActivity.kt
@@ -1,16 +1,21 @@
 package dev.jmoicano.newsapp.sourceslist.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import dev.jmoicano.newsapp.data.Result
 import dev.jmoicano.newsapp.sourceslist.data.models.SourceResponse
@@ -68,5 +73,21 @@ fun SourcesList(result: Result<List<SourceResponse>>) {
 
 @Composable
 fun SourceView(sourceResponse: SourceResponse) {
-    Text(text = sourceResponse.name)
+    val context = LocalContext.current
+    Card(
+        modifier = Modifier.padding(10.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colors.onSurface)
+    ) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = sourceResponse.name, style = MaterialTheme.typography.h4)
+            Text(text = sourceResponse.description, style = MaterialTheme.typography.body2)
+            TextButton(onClick = {
+                val intent =
+                    Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(sourceResponse.url) }
+                context.startActivity(intent)
+            }) {
+                Text(text = sourceResponse.url, style = MaterialTheme.typography.caption)
+            }
+        }
+    }
 }
